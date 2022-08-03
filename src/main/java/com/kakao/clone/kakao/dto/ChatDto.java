6c8386cfa90f264fb62079f8cf40fc3f7b80565f
jjucc99 <59110017+jjucc99@users.noreply.github.com>
2022-08-03 23:24:30 +0900

    Revert "Revert "Merge pull request #7 from jjucc99/BE""
    This reverts commit 754de3b7c90b9266f59a3813a422fbdc4b68dc61.

--- src/main/java/com/kakao/clone/kakao/dto/ChatDto.java
+++ src/main/java/com/kakao/clone/kakao/dto/ChatDto.java
@@ -7,8 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserDto {
-    private String username;
+public class ChatDto {
     private String participants;
     private String roomName;
 }
