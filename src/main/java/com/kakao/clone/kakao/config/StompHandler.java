40ae26cb2b31141e1e2e53563797b69d99f805f2
jjucc99 <59110017+jjucc99@users.noreply.github.com>
2022-08-04 11:38:18 +0900

    update

--- src/main/java/com/kakao/clone/kakao/config/StompHandler.java
+++ src/main/java/com/kakao/clone/kakao/config/StompHandler.java
@@ -1,16 +1,17 @@
-package com.kakao.clone.kakao.security;
+package com.kakao.clone.kakao.config;
 
 import com.kakao.clone.kakao.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
+@Component
 public class StompHandler implements ChannelInterceptor {
     private final JwtTokenProvider tokenProvider;
 
--- src/main/java/com/kakao/clone/kakao/config/StompWebSocketConfig.java
+++ src/main/java/com/kakao/clone/kakao/config/StompWebSocketConfig.java
@@ -1,38 +1,50 @@
 package com.kakao.clone.kakao.config;
 
-import com.kakao.clone.kakao.security.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @EnableWebSocketMessageBroker
 @Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE + 99)
+@RequiredArgsConstructor
 public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    //endpoint를 /stomp로 하고, allowedOrigins를 "*"로 하면 페이지에서
-    //Get /info 404 Error가 발생한다. 그래서 아래와 같이 2개의 계층으로 분리하고
-    //origins를 개발 도메인으로 변경하니 잘 동작하였다.
-    //이유는 왜 그런지 아직 찾지 못함
+    private final StompHandler stompHandler;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/stomp")
                 .setAllowedOriginPatterns("*");
-//                .withSockJS();
+//                .withSockJS()
+//                .setWebSocketEnabled(false)
+//                .setSessionCookieNeeded(false);
+        System.out.println("stomp1");
     }
 
     /*어플리케이션 내부에서 사용할 path를 지정할 수 있음*/
+    // Client 에서 SEND 요청을 처리
+    //Spring docs 에서는 /topic, /queue로 나오나 편의상 /pub, /sub로 변경
+    //해당 경로로 SimpleBroker를 등록.
+    // SimpleBroker는 해당하는 경로를 SUBSCRIBE하는 Client에게 메세지를 전달하는 간단한 작업을 수행
+    //enableStompBrokerRelay
+    //SimpleBroker의 기능과 외부 Message Broker( RabbitMQ, ActiveMQ 등 )에 메세지를 전달하는 기능을 가짐
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // Client 에서 SEND 요청을 처리
-        //Spring docs 에서는 /topic, /queue로 나오나 편의상 /pub, /sub로 변경
-        registry.setApplicationDestinationPrefixes("/pub");
-        //해당 경로로 SimpleBroker를 등록.
-        // SimpleBroker는 해당하는 경로를 SUBSCRIBE하는 Client에게 메세지를 전달하는 간단한 작업을 수행
+        System.out.println("stomp2");
         registry.enableSimpleBroker("/sub");
-        //enableStompBrokerRelay
-        //SimpleBroker의 기능과 외부 Message Broker( RabbitMQ, ActiveMQ 등 )에 메세지를 전달하는 기능을 가짐
+        registry.setApplicationDestinationPrefixes("/pub");
+
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
     }
 }
